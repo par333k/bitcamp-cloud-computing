19071aa33135a7d14868e4a2baea8198e4847852
par333k <par333k@naver.com>
2018-07-13 18:47:59 +0900

    .

---
+++ bitcamp-web-10/src/main/java/bitcamp/pms/config/AppConfig.java
@@ -0,0 +1,25 @@
+package bitcamp.pms.config;
+
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    public AppConfig() {
+        System.out.println("AppConfig() 호출됨!");
+    }
+
+    /*@Bean("sqlSessionFactory")
+    public SqlSessionFactory getSqlSessionFactory() throws Exception {
+        System.out.println("AppConfig.getSqlSessionFactory() 호출됨!");
+        String resource = "bitcamp/pms/config/mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        return new SqlSessionFactoryBuilder().build(inputStream);
+
+    }*/
+}
---
+++ bitcamp-web-10/src/main/webapp/board/updatefail.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" 
+    contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>    
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset='UTF-8'>
+<meta http-equiv="Refresh" content="1;url=list">
+<title>게시물 변경</title>
+</head>
+<body>
+<h1>게시물 변경 오류!</h1>
+<p>해당 게시물이 없습니다!</p>
+</body>
+</html>
+    
